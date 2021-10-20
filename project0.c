1b3d142877b6c2cdef7c843fc4754492abc2bc01
Nima Shadaram <60158066+nimashad16@users.noreply.github.com>
2021-10-20 16:37:45 -0500

    Update project0.c

--- project0.c
+++ project0.c
@@ -1,4 +1,3 @@
-
 //
 //  main.c
 //  Project 0
@@ -86,12 +85,12 @@ void printValues(struct element values[], int sizeOfStruct){
 int main(int argc, char **argv){
     
     struct element *UTFG = malloc(MAX_SIZE * sizeof(struct element));    //This will point to the variable in the memory
-     memset(UTFG, 0, sizeof(UTFG));
+    
     char letter;
      int result;                                     //stores byte size result
      int memory = 0;                                    //Accesses the struct variables from the memory
      int compare = 0;                               //int variable to compare 2 unicode characters
-     int x;                                         //reads in first byte of unicode character
+     int a;                                         //reads in first byte of unicode character
      letter = fgetc(stdin);                         //Gets the letter of the standard input
      char secChar;                                   //Stores the bytes of the characters
      char thirdChar;
@@ -99,7 +98,7 @@ int main(int argc, char **argv){
     
     
    
-   
+    memset(UTFG, 0, sizeof(UTFG));
     while (letter  != EOF) {                //This will determine the number of bytes to be assigned
         if ((unsigned char) letter < 192){
             result = 1;
@@ -126,26 +125,26 @@ int main(int argc, char **argv){
             fourthChar = fgetc(stdin);
         }
            
-            for (x = 0; x < memory; x++) {              //Check to determine if the unicode was already put in
+            for (a = 0; a < memory; a++) {              //Check to determine if the unicode was already put in
                 if (result == 1){
-                    compare = (UTFG[x].byte_size1 == (unsigned char) letter);
+                    compare = (UTFG[a].byte_size1 == (unsigned char) letter);
                 }
                 if (result == 2){
-                    compare = ((UTFG[x].byte_size1 == (unsigned char) letter) & ((UTFG[x].byte_size2) == (unsigned char) secChar));
+                    compare = ((UTFG[a].byte_size1 == (unsigned char) letter) & ((UTFG[a].byte_size2) == (unsigned char) secChar));
                 }
                 if (result == 3){
-                    compare = (UTFG[x].byte_size1 == (unsigned char) letter) & (UTFG[x].byte_size2 == (unsigned char) secChar) & (UTFG[x].byte_size3 == (unsigned char) thirdChar);
+                    compare = (UTFG[a].byte_size1 == (unsigned char) letter) & (UTFG[a].byte_size2 == (unsigned char) secChar) & (UTFG[a].byte_size3 == (unsigned char) thirdChar);
                 }
                 if (result == 4){
-                    compare = (UTFG[x].byte_size1 == (unsigned char) letter) & (UTFG[x].byte_size2 == (unsigned char) secChar) &
-                          (UTFG[x].byte_size3 == (unsigned char) thirdChar) & (UTFG[x].byte_size4 == (unsigned char) fourthChar);
+                    compare = (UTFG[a].byte_size1 == (unsigned char) letter) & (UTFG[a].byte_size2 == (unsigned char) secChar) &
+                          (UTFG[a].byte_size3 == (unsigned char) thirdChar) & (UTFG[a].byte_size4 == (unsigned char) fourthChar);
                 }
                 if (compare == 1){
                     break;
                 }
             }
         if (compare == 1){                           //Increases the count of unicode characters
-                UTFG[x].counter++;
+                UTFG[a].counter++;
         }
         else {
                 UTFG[memory].counter++;
