4d998cf60968e86009b2bc50b4e7831a1eb9cc27
Nima Shadaram <60158066+nimashad16@users.noreply.github.com>
2021-10-20 18:04:18 -0500

    Update project0.c

--- project0.c
+++ project0.c
@@ -77,7 +77,6 @@ void printValues(struct element values[], int sizeOfStruct){                    
       
         unsigned char *characters = &byteArray[0];
         
-     
         printf("%s", characters);                                       //Will print the characters and their values
         printf("->");
         printf("%d\n", values[a].counter);
@@ -127,7 +126,7 @@ int main(int argc, char **argv){
             fourthChar = fgetc(stdin);
         }
            
-            for (x = 0; x < memory -1; x++) {              //Check to determine if the unicode was already put in
+            for (x = 0; x < memory; x++) {              //Check to determine if the unicode was already put in
                 if (result == 1){
                     compare = (UTFG[x].byte_size1 == (unsigned char) letter);
                 }
